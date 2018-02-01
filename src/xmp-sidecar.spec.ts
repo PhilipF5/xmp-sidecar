9b6260a1e045df723c50787561c9e2da4d5228f1
Philip Fulgham <philfulgham@gmail.com>
2018-01-31 20:25:56 -0500

    test: add test coverage for new properties and methods

--- src/xmp-sidecar.spec.ts
+++ src/xmp-sidecar.spec.ts
@@ -23,3 +23,48 @@ describe("XmpSidecar constructor", () => {
 		expect(() => { new XmpSidecar("fake-file.xmp"); }).to.throw();
 	});
 });
+
+describe("Property getters", () => {
+	const obj = new XmpSidecar("../test.xmp");
+	it("should have a rating of 4", () => {
+		expect(obj.rating).to.equal(4);
+	});
+	it("should have the tags 'google stuff', 'computer', 'home'", () => {
+		expect(obj.tags.includes("google stuff")).to.be.true;
+		expect(obj.tags.includes("computer")).to.be.true;
+		expect(obj.tags.includes("home")).to.be.true;
+	});
+});
+
+describe("Property setters", () => {
+	const obj = new XmpSidecar("../test.xmp");
+	it("should change the rating to 2", () => {
+		obj.rating = 2;
+		expect(obj.rating).to.equal(2);
+	});
+	it("should add the tag 'tardis'", () => {
+		obj.addTag("tardis");
+		expect(obj.tags.includes("tardis")).to.be.true;
+	});
+	it("should not duplicate the tag 'home'", () => {
+		obj.addTag("home");
+		let count = 0;
+		obj.tags.forEach((item) => {
+			if (item === "home") {
+				count++;
+			}
+		});
+		expect(count).to.equal(1);
+	});
+	it("should remove the tag 'google stuff'", () => {
+		obj.removeTag("google stuff");
+		expect(obj.tags.includes("google stuff")).to.be.false;
+	});
+});
+
+describe("Static methods", () => {
+	it("should initialize new XmpSidecar object from file", () => {
+		const obj = XmpSidecar.load("../test.xmp");
+		expect(obj).to.not.be.null;
+	});
+});
